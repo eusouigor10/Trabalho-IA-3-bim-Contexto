a75d5f4cd951bd249cd38d5848af41cf71b38b52
Igor Daré <eusouigor10@gmail.com>
2026-08-26 16:56:32 -0300

    adição da função de cálculo da distância e instrução de download dos embeddings

---
+++ embedding_download.md
@@ -0,0 +1,4 @@
+# Para baixar o arquivo de embeddings:
+1. Acesse https://huggingface.co/nilc-nlp/fasttext-skip-gram-50d/tree/main
+2. Baixe "embeddings.safetensors" e "vocab.txt"
+3. Coloque na pasta embedding/
--- main.py
+++ main.py
@@ -10,4 +10,13 @@ texto = server.tokenizacao(texto)
 
 texto = server.remocao_stopwords(texto)
 
-server.contagem_filtragem_frequencia(texto)
+vocabulario = server.contagem_filtragem_frequencia(texto)
+
+embeddings = server.criar_embeddings(vocabulario)
+
+palavra_sorteada = server.sorteio_palavra(vocabulario)
+
+distancias = server.calculo_distancias(palavra_sorteada, embeddings)
+
+print(palavra_sorteada)
+print(distancias)
